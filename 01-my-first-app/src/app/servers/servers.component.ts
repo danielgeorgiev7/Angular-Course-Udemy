ffcc049af9f8a33366f5ba2538ef4df390a9abd9
nymuelis <daniel.tsv.georgiev@gmail.com>
2024-02-14 22:08:36 +0200

    Add second assignment

--- 01-my-first-app/src/app/servers/servers.component.ts
+++ 01-my-first-app/src/app/servers/servers.component.ts
@@ -1,12 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   // selector: '[app-servers]',
   selector: 'app-servers',
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>
-  `,
+  // template: `
+  //   <app-server></app-server>
+  //   <app-server></app-server>
+  // `,
+  templateUrl: './servers.component.html',
   styleUrl: './servers.component.css',
 })
-export class ServersComponent {}
+export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  userCreationStatus = 'No user was created!';
+  serverName = 'Testserver';
+  username = '';
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus =
+      'Server was created! Name is ' + this.serverName;
+  }
+
+  ngOnInit(): void {}
+
+  onUpdateServerName(event: any) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+  onCreateUser() {
+    this.userCreationStatus = 'User was created! Username: ' + this.username;
+  }
+
+  onUpdateUsername(event: any) {
+    this.username = (<HTMLInputElement>event.target).value;
+  }
+
+  resetUsername() {
+    this.username = '';
+    this.userCreationStatus = 'Username was reset! Type a new username!';
+    setTimeout(() => {
+      this.userCreationStatus = 'No user was created!';
+    }, 4000);
+  }
+}
