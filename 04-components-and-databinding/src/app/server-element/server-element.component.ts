9fe2442c03961e6262e01d3150a128370f05471a
nymuelis <daniel.tsv.georgiev@gmail.com>
2024-02-22 03:01:09 +0200

    components and databinding pt 1

--- 04-components-and-databinding/src/app/server-element/server-element.component.ts
+++ 04-components-and-databinding/src/app/server-element/server-element.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrl: './server-element.component.css'
+  styleUrl: './server-element.component.css',
 })
 export class ServerElementComponent {
-
+  @Input('serverElement') element: {
+    type: string;
+    name: string;
+    content: string;
+  };
 }
