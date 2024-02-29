3ac54d0b6c53b9ddde3fc1b5ccef3dac48fbee17
nymuelis <daniel.tsv.georgiev@gmail.com>
2024-03-01 01:35:02 +0200

    Add guards and data resolving

--- 07-routing/src/app/app.module.ts
+++ 07-routing/src/app/app.module.ts
@@ -10,9 +10,13 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
-import { RouterModule, Routes } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { AppRoutingModule } from './app-routing.module';
+import { CanDeactivateGuard } from './servers/edit-server/can-deactivate-guard.service';
+import { AuthService } from './auth.service';
+import { AuthGuard } from './auth-guard.service';
+import { ErrorPageComponent } from './error-page/error-page.component';
+import { ServerResolver } from './servers/server/server-resolver.service';
 
 @NgModule({
   declarations: [
@@ -24,9 +28,16 @@ import { AppRoutingModule } from './app-routing.module';
     EditServerComponent,
     ServerComponent,
     PageNotFoundComponent,
+    ErrorPageComponent,
   ],
   imports: [BrowserModule, FormsModule, AppRoutingModule],
-  providers: [ServersService],
+  providers: [
+    ServersService,
+    AuthService,
+    AuthGuard,
+    CanDeactivateGuard,
+    ServerResolver,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
