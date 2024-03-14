22a4e849264fe90eb88dbb2dc6c0e345d796dec0
nymuelis <daniel.tsv.georgiev@gmail.com>
2024-03-14 23:29:29 +0200

    Add auth interceptor for http params

--- 03-course-project/src/app/app.component.ts
+++ 03-course-project/src/app/app.component.ts
@@ -1,15 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './auth/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = '03-basics-project';
-  loadedFeature = 'recipe';
+export class AppComponent implements OnInit {
+  constructor(private authService: AuthService) {}
 
-  onNavigate(feature: string) {
-    this.loadedFeature = feature;
+  ngOnInit(): void {
+    this.authService.autoLogin();
   }
 }
