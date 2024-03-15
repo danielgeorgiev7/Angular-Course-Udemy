93bc72db5804409d3569a575ea1b306cf1421e6c
nymuelis <daniel.tsv.georgiev@gmail.com>
2024-03-15 20:39:39 +0200

    Add deployment

--- 03-course-project/src/app/auth/auth.service.ts
+++ 03-course-project/src/app/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject, catchError, tap, throwError } from 'rxjs';
 import { User } from './user.model';
 import { Router } from '@angular/router';
+import { environment } from '../../environments/environment';
 
 export interface AuthResponseData {
   kind: string;
@@ -26,7 +27,7 @@ export class AuthService {
   signup(email: string, password: string) {
     return this.http
       .post<AuthResponseData>(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBecfaaPBVOp8fQx0ze76p-C8JubfxHMD4',
+        `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${environment.firebaseAPIKey}`,
         { email, password, returnSecureToken: true }
       )
       .pipe(
@@ -45,7 +46,7 @@ export class AuthService {
   login(email: string, password: string) {
     return this.http
       .post<AuthResponseData>(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBecfaaPBVOp8fQx0ze76p-C8JubfxHMD4',
+        `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${environment.firebaseAPIKey}`,
         { email, password, returnSecureToken: true }
       )
       .pipe(
---
+++ 03-course-project/src/environments/environment.development.ts
@@ -0,0 +1,3 @@
+export const environment = {
+  firebaseAPIKey: 'AIzaSyBecfaaPBVOp8fQx0ze76p-C8JubfxHMD4',
+};
